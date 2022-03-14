818efe8986bb7e2c8ce73d1f2a74663d77adf478
apocpax <richard.c.lummus90@gmail.com>
2022-03-14 09:39:34 -0700

    second commit

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,16 +1,74 @@
-import React from "react";
+import React, { createRef } from "react";
 import HeadTags from "./HeadTags";
 import Navbar from "./Navbar";
-import { Container } from "semantic-ui-react";
+import {
+  Container,
+  Visibility,
+  Grid,
+  Sticky,
+  Ref,
+  Divider,
+  Segment
+} from "semantic-ui-react";
+import nprogress from "nprogress";
+import Router,{useRouter}  from "next/router";
+import SideMenu from "./SideMenu";
+import Search from "./Search";
 
-function Layout() {
+function Layout({ children, user }) {
+  const contextRef = createRef();
+  const router=useRouter();
+
+   const messagesRoute = router.pathname === "/messages";
+
+  Router.onRouteChangeStart = () => nprogress.start();
+  Router.onRouteChangeComplete = () => nprogress.done();
+  Router.onRouteChangeError = () => nprogress.done();
+
   return (
     <>
       <HeadTags />
+      {user ? (
+        <div style={{ marginLeft: "1rem", marginRight: "1rem" }}>
+          <Ref innerRef={contextRef}>
+            <Grid>
+              {!messagesRoute ? (
+                <>
+              <Grid.Column floated="left" width={2}>
+                <Sticky context={contextRef}>
+                  <SideMenu user={user} />
+                </Sticky>
+              </Grid.Column>
 
-      <Navbar />
+              <Grid.Column width={10}>
+                <Visibility context={contextRef}>{children}</Visibility>
+              </Grid.Column>
 
-      <Container style={{ paddingTop: "1rem" }} text></Container>
+              <Grid.Column floated="left" width={4}>
+                <Sticky context={contextRef}>
+                  <Segment basic>
+                    <Search />
+                  </Segment>
+                </Sticky>
+              </Grid.Column>
+                 </>
+              ) : (
+                <>
+                  <Grid.Column floated="left" width={1} />
+                  <Grid.Column width={15}>{children}</Grid.Column>
+                </>
+              )}
+            </Grid>
+          </Ref>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Container text style={{ paddingTop: "1rem" }}>
+            {children}
+          </Container>
+        </>
+      )}
     </>
   );
 }
--- utilsServer/connectDb.js
+++ utilsServer/connectDb.js
@@ -5,8 +5,7 @@ async function connectDb() {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+
     });
     console.log("Mongodb connected");
   } catch (error) {
